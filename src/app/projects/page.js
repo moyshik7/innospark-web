1b1bfd3c29ba897820a6e739d33c8e4e82a5ec81
Oyshik <moyshik7@gmail.com>
2023-12-28 23:43:50 +0600

    Added shit to shit

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,49 +1,49 @@
 import { Inter } from 'next/font/google';
-import { Header } from '@/components/home/header/header';
 import { ProjectsContainer } from '@/components/projects/projectContainer';
+import DisplayCards from '@/components/home/cards/displayCards';
 
 export const metadata = {
-  title: 'InnoSpark',
-  description: 'Leading digital marketing service in Bangladesh',
+    title: 'InnoSpark',
+    description: 'Leading digital marketing service in Bangladesh',
 
-  metadataBase: new URL('https://innosparkbd.com'),
+    metadataBase: new URL('https://innosparkbd.com'),
   
-  keywords: ['Next.js', 'React', 'JavaScript'],
-  openGraph: {
-    title: 'InnoSpark',
-    description: 'Leading digital marketing service in Bangladesh',
-    url: 'https://innosparkbd.com',
-    siteName: 'InnoSpark',
-    images: 'https://images2.imgbox.com/57/71/NYnzgUOV_o.png',
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: true, 
-    follow: true,
+    keywords: ['Next.js', 'React', 'JavaScript'],
+    openGraph: {
+        title: 'InnoSpark',
+        description: 'Leading digital marketing service in Bangladesh',
+        url: 'https://innosparkbd.com',
+        siteName: 'InnoSpark',
+        images: 'https://images2.imgbox.com/57/71/NYnzgUOV_o.png',
+        locale: 'en_US',
+        type: 'website',
+    },
+    robots: {
+        index: true, 
+        follow: true,
 
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'InnoSpark',
-    description: 'Leading digital marketing service in Bangladesh',
-    creator: '@akiorochi',
-    images: ['https://images2.imgbox.com/57/71/NYnzgUOV_o.png'],
-  },
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'InnoSpark',
+        description: 'Leading digital marketing service in Bangladesh',
+        creator: '@akiorochi',
+        images: ['https://images2.imgbox.com/57/71/NYnzgUOV_o.png'],
+    },
 }
 
 export const viewport = {
-  themeColor: '#ff6f61',
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  colorScheme: 'dark',
+    themeColor: '#ff6f61',
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    colorScheme: 'dark',
 }
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (<>
-    <ProjectsContainer/>
-  </>)
+    return (<>
+        <ProjectsContainer/>
+    </>)
 }
--- src/app/service/page.jsx
+++ src/app/service/page.jsx
@@ -1,48 +1,49 @@
 import { Inter } from 'next/font/google';
 import { Service } from '@/components/service/service';
+import DisplayCards from '@/components/home/cards/displayCards';
 
 export const metadata = {
-  title: 'InnoSpark',
-  description: 'Leading digital marketing service in Bangladesh',
+    title: 'InnoSpark',
+    description: 'Leading digital marketing service in Bangladesh',
 
-  metadataBase: new URL('https://innosparkbd.com'),
+    metadataBase: new URL('https://innosparkbd.com'),
   
-  keywords: ['Next.js', 'React', 'JavaScript'],
-  openGraph: {
-    title: 'InnoSpark',
-    description: 'Leading digital marketing service in Bangladesh',
-    url: 'https://innosparkbd.com',
-    siteName: 'InnoSpark',
-    images: 'https://images2.imgbox.com/57/71/NYnzgUOV_o.png',
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: true, 
-    follow: true,
-
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'InnoSpark',
-    description: 'Leading digital marketing service in Bangladesh',
-    creator: '@akiorochi',
-    images: ['https://images2.imgbox.com/57/71/NYnzgUOV_o.png'],
-  },
+    keywords: ['Next.js', 'React', 'JavaScript'],
+    openGraph: {
+        title: 'InnoSpark',
+        description: 'Leading digital marketing service in Bangladesh',
+        url: 'https://innosparkbd.com',
+        siteName: 'InnoSpark',
+        images: 'https://images2.imgbox.com/57/71/NYnzgUOV_o.png',
+        locale: 'en_US',
+        type: 'website',
+    },
+    robots: {
+        index: true, 
+        follow: true,
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'InnoSpark',
+        description: 'Leading digital marketing service in Bangladesh',
+        creator: '@akiorochi',
+        images: ['https://images2.imgbox.com/57/71/NYnzgUOV_o.png'],
+    },
 }
 
 export const viewport = {
-  themeColor: '#ff6f61',
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  colorScheme: 'dark',
+    themeColor: '#ff6f61',
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    colorScheme: 'dark',
 }
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (<>
-    <Service/>
-  </>)
+    return (<>
+        <DisplayCards/>
+        <Service/>
+    </>)
 }
