46e5188d264f91cb07462dc810335402c83308e8
Oyshik <moyshik7@gmail.com>
2023-12-28 23:37:15 +0600

    Added tos page

---
+++ src/components/tos/tosholder.jsx
@@ -0,0 +1,71 @@
+"use client"
+
+
+export const TosHolder = () => {
+    return(<>
+        <div className="p-5 text-left mt-20 text-gray-600 dark:text-gray-400 font-mono font-semibold mb-52">
+<h1 className="text-3xl mb-10">Terms of Service</h1>
+
+
+<h2 className="text-xl mt-10">1. Acceptance of Terms</h2>
+<p>
+Welcome to InnoSpark&apos;s website (innosparkbd.com). By accessing or using our services, including but not limited to digital marketing, video editing, web development, motion graphics, animation, and search engine optimization, you agree to comply with and be bound by the following Terms of Service. If you do not agree to these terms, please do not use our services.
+</p>
+
+
+<h2 className="text-xl mt-10">2. Services</h2>
+<p>
+InnoSpark provides a comprehensive array of services through its website, each designed to meet the diverse needs of our clients. The specifics of each service, including terms, conditions, and pricing, may be outlined in separate agreements or communicated directly through our platform. We strive to deliver high-quality solutions and are committed to ensuring your satisfaction.
+</p>
+
+
+<h2 className="text-xl mt-10">3. User Conduct</h2>
+<p>
+You agree to use our services responsibly, in accordance with all applicable laws and regulations. Users are solely responsible for any content they create, submit, or display on our platform. InnoSpark reserves the right to terminate services and take appropriate legal action if any user engages in conduct that violates these terms.
+</p>
+
+
+<h2 className="text-xl mt-10">4. Intellectual Property</h2>
+<p>
+All content and materials provided by InnoSpark, including but not limited to text, graphics, logos, and software, are the property of InnoSpark and are protected by intellectual property laws. Users are not permitted to reproduce, distribute, or create derivative works without explicit permission. Any trademarks or logos displayed on our website are the property of their respective owners.
+</p>
+
+
+<h2 className="text-xl mt-10">5. Privacy Policy</h2>
+<p>
+By using our services, you agree to the terms outlined in our Privacy Policy, which can be found on our website. We are committed to protecting your privacy and handling your personal information responsibly. Our Privacy Policy details the types of information we collect, how it is used, and the measures taken to safeguard your data.
+</p>
+
+
+
+<h2 className="text-xl mt-10">6. Disclaimer of Warranties</h2>
+<p>
+InnoSpark provides its services on an &apos;as-is&apos; and &apos;as-available&apos; basis. While we strive for the highest quality, we do not guarantee that our services will be uninterrupted or error-free. We disclaim all warranties, expressed or implied, including but not limited to the implied warranties of merchantability and fitness for a particular purpose.
+</p>
+
+
+<h2 className="text-xl mt-10">7. Limitation of Liability</h2>
+<p>
+InnoSpark shall not be liable for any direct, indirect, incidental, special, or consequential damages arising out of the use or inability to use our services. Users agree to indemnify and hold InnoSpark harmless from any claims or demands, including legal fees.
+</p>
+
+
+<h2 className="text-xl mt-10">8. Modifications to Terms</h2>
+<p>
+InnoSpark reserves the right to modify these Terms of Service at any time. It is the user&apos;s responsibility to review the terms regularly. Continued use of our services after modifications constitutes acceptance of the updated terms. We may notify users of significant changes through email or prominent announcements on our website.
+</p>
+
+
+<h2 className="text-xl mt-10">9. Governing Law</h2>
+<p>
+These Terms of Service are governed by the laws of [Your Jurisdiction]. Any legal action or proceeding related to these terms shall be brought exclusively in the courts of [Your Jurisdiction]. Both parties agree to submit to the personal jurisdiction of the courts in [Your Jurisdiction].
+</p>
+
+
+<h2 className="text-xl mt-10">10. Contact Information</h2>
+<p>
+If you have any questions or concerns regarding these Terms of Service, please contact us at [your email address]. We value your feedback and are here to address any inquiries you may have.
+</p>
+        </div>
+    </>)
+}
