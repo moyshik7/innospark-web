6bb7b485b841ca86bb8dbd7f33ce92f78d6f7cb9
Oyshik <moyshik7@gmail.com>
2023-12-28 22:49:33 +0600

    Fix 101

--- src/components/home/cards/list/displayCard1.jsx
+++ src/components/home/cards/list/displayCard1.jsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 
 export const GenerateDisplayCardLeft = ({image, title, description}) => {
     return (
-        <div className="flex items-center justify-center m-0 mt-20 mb-20 border rounded-xl p-20 lg:m-20">
-            <div className="shadow-lg rounded-lg overflow-hidden">
+        <div className="flex shadow-lg items-center justify-center m-0 mt-20 mb-20 border rounded-xl p-20 lg:m-20">
+            <div className="overflow-hidden">
                 <div className="lg:flex lg:justify-between">
                     <div className="p-8 lg:flex-shrink-0 w-full lg:w-1/3">
                         <Image
@@ -31,8 +31,8 @@ export const GenerateDisplayCardLeft = ({image, title, description}) => {
 
 export const GenerateDisplayCardRight = ({image, title, description}) => {
     return (
-        <div className="flex items-center justify-center m-0 mt-20 mb-20 border rounded-xl p-20 lg:m-20">
-            <div className="shadow-lg rounded-lg overflow-hidden">
+        <div className="flex shadow-lg items-center justify-center m-0 mt-20 mb-20 border rounded-xl p-20 lg:m-20">
+            <div className="overflow-hidden">
                 <div className="lg:flex lg:justify-between">
                     <div className="p-8 flex flex-col items-center justify-center w-full lg:w-1/3 space-a text-center">
                         <div className="tracking-wide text-2xl text-indigo-500 font-semibold">
--- src/components/home/cards/list/whycard.jsx
+++ src/components/home/cards/list/whycard.jsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 
 export const GenerateWhyCard = ({q, a}) => {
     return (
-        <div className="flex items-center justify-center m-0 mt-20 mb-20 border rounded-2xl p-10 md:p-20 lg:p-40 lg:m-20">
-            <div className="shadow-lg rounded-lg overflow-hidden">
+        <div className="flex shadow-lg items-center justify-center m-0 mt-20 mb-20 border rounded-2xl p-10 md:p-20 lg:p-40 lg:m-20">
+            <div className="overflow-hidden">
                 <div className="p-8 flex flex-col items-center justify-center w-full space-a  text-center">
                     <div className="tracking-wide text-3xl text-indigo-500 font-semibold">
                         {q}
