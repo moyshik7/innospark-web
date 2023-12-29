36bc8c48e5151e3679f5f7f068f1739b44e7d733
Oyshik <moyshik7@gmail.com>
2023-12-30 01:55:39 +0600

    Add graphic designer

--- src/components/home/cards/displayCards.jsx
+++ src/components/home/cards/displayCards.jsx
@@ -16,22 +16,28 @@ const DisplayCards = () => {
             image="/images/banners/digital-marketing.jpg"
         />
 
-
         <GenerateDisplayCardRight 
+            title="Graphics Designer" 
+            description={`Elevate your brand with our expertly crafted visual solutions. From sleek logos to impactful marketing materials, our designs seamlessly blend creativity and functionality. Experience the epitome of design excellence for a lasting brand impression`} 
+            image="/images/banners/graphics.webp"
+        />
+
+
+        <GenerateDisplayCardLeft 
             title="Web Development" 
             description={`A website is essential for any business. It helps you reach a wider audience, establish your brand, and provide important information about your business. It can also help to increase sales and revenue`} 
             image="/images/banners/web-dev.jpg"
         />
         
 
-        <GenerateDisplayCardLeft 
+        <GenerateDisplayCardRight
             title="Search Engine Optimization" 
             description={`Ranking in search engines specially in google search is important for businesses. We offer search engine optimization services that cover latest changes in the domain.`} 
             image="/images/banners/seo.webp"
         />
 
 
-        <GenerateDisplayCardRight 
+        <GenerateDisplayCardLeft
             title="App Development" 
             description={`Mobile application development is the method of making software applications that run on a mobile device`} 
             image="/images/banners/app.webp"
@@ -48,7 +54,29 @@ const DisplayCards = () => {
 
 
 
-        <GenerateStatsCard q="aaa" a="aaa"/>
+        <div className="block lg:flex flex-row flex-wrap">
+            <GenerateStatsCard
+                q="01. Data + Brainstorming" 
+                a="Data takes the spotlight as the ultimate key to our success! We're armed with a treasure trove of market data collected over years across a myriad of industries. By blending this historical industry wisdom and competitor insights with your current business data, we kick off a cheerful brainstorming session on how to achieve some seriously awesome results!ll lg:w-1/2" 
+                image="/images/box/logo-018.webp"
+            />
+            <GenerateStatsCard 
+                q="02. Strategy + Execution" 
+                a="In the exciting realm of business growth, our secret sauce involves crafting vibrant, custom campaign strategies driven by data! We pride ourselves on infusing every strategy with the power of data, ensuring adaptability for a future that's as bright as your business aspirations. Picture our execution plan as a lively roadmap, filled with targets, goals, milestones, tasks, and deadlines – because we believe in turning every business journey into a cheerful adventure!" 
+                image="/images/box/logo-019.webp"
+            />
+            <GenerateStatsCard 
+                q="03. Consistency + Quality" 
+                a="In the delightful dance of execution, quality, and consistency, success is sure to take center stage! Our specially crafted Standard Operating Procedures (SOP) and cutting-edge automation, which is the talk of the industry, join forces to ensure unwavering consistency. Think of our work process as a symphony of checklists, with extensive automation sprinkled at every level, guaranteeing not just results, but results that sparkle with top-notch quality!" 
+                image="/images/box/logo-020.webp"
+            />
+            <GenerateStatsCard 
+                q="04. KPI Tracking + Optimization" 
+                a="We're on a joyous mission, setting up advanced KPIs that gleefully track every move, even diving into the delightful world of behavior analysis to witness how our services are spreading joy and growth throughout your business. With a continuous dance of tracking and measurement involving over 50+ different data points, we joyfully fine-tune the campaign, aiming for results that sparkle with success!" 
+                image="/images/box/logo-021.webp"
+            />
+        </div>
+        
     </>);
 };
 
--- src/components/home/cards/list/statsCard.jsx
+++ src/components/home/cards/list/statsCard.jsx
@@ -2,15 +2,22 @@
 import React from 'react';
 import Image from "next/image"
 
-export const GenerateStatsCard = ({q, a}) => {
+export const GenerateStatsCard = ({q, a, image}) => {
     return (
-        <div className="flex shadow-lg items-center justify-center m-0 mt-20 mb-20 border rounded-2xl p-10 md:p-20 lg:p-40 w-1/2">
+        <div className="flex items-center justify-center m-0 mt-20 mb-20 p-10 md:p-20 md:pb-10 md:pt-10 lg:p-40 lg:pt-10 lg:pb-10 w-full lg:w-1/2">
             <div className="overflow-hidden">
                 <div className="p-8 flex flex-col items-center justify-center w-full space-a  text-center">
-                    <div className="tracking-wide text-3xl text-indigo-500 font-semibold">
+                    <Image
+                        src={image}
+                        alt={a}
+                        width={1024}
+                        height={1024}
+                        className="w-full"
+                    />
+                    <h3 className="tracking-wide text-2xl text-left text-gray-500 font-semibold w-full">
                         {q}
-                    </div>
-                    <p className="mt-10 text-xl text-gray-500">
+                    </h3>
+                    <p className="mt-10 text-lg text-gray-500 text-left w-full">
                         {a}
                     </p>
                 </div>
