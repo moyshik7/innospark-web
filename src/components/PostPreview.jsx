ec08277c81644c926fec40aae02c29b688dfd746
Oyshik <moyshik7@gmail.com>
2024-01-09 18:10:48 +0600

    remove comments

--- src/components/PostPreview.jsx
+++ src/components/PostPreview.jsx
@@ -2,23 +2,8 @@
 import Link from 'next/link';
 import { Inset } from '@radix-ui/themes';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-/*
-const PostPreview = (props) => {
-  return (
-    <div
-      className="border border-slate-300 p-4 rounded-md shadow-sm
-    bg-white"
-    >
-      <p className="text-sm text-slate-400">{props.date}</p>
 
-      <Link href={`/posts/${props.slug}`}>
-        <h2 className=" text-violet-600 hover:underline mb-4">{props.title}</h2>
-      </Link>
-      <p className="text-slate-700">{props.subtitle}</p>
-    </div>
-  );
-};
-*/
+
 const PostPreview = (props) => {
     return (
         <Link href={`/posts/${props.slug}`} key={props.slug} className="h-full">
